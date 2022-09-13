5c7b9d8cb706a182d4909272b7e71a6e7c7acf09
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-13 15:46:26 -0500

    Add ability to parse files to the 2nd pattern of wording while processing client invalid login attempts

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -6,5 +6,7 @@ cd $client_dir
 
 touch failed_login_data.txt
 
-cat $client_dir/* | awk 'match($0, /([a-zA-Z]+) \([0-9]+) \([0-9]+):..:.. computer_name sshd[.....]: Failed password for invalid user \([a-zA-Z]+) from \([0-9.]+) .*/, groups) {print groups[1] groups[2] groups[3] groups[4] groups[5] "\n"}' > failed_login_data.txt
+cat */*/* | awk 'match($0, /([a-zA-Z0-9 ]+):..:.. computer_name sshd[.....]: Failed password for invalid user \([a-zA-Z]+) from \([0-9.]+) */, groups) {print groups[1] groups[2] groups[3] "\n"}' > failed_login_data.txt
+
+cat */*/* | awk 'match($0, /([a-zA-Z0-9 ]+):..:.. computer_name sshd[.....]: Failed password for \([a-zA-Z]+) from \([0-9.]+) */, groups) {print groups[1] groups[2] groups[3] "\n"}' > failed_login_data.txt
 
