ec885dc56453ee033ddf49394ecc60a401a13759
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-14 12:27:57 -0500

    Alter client log files to pass bats tests: 	-combine two patterns searches

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -4,9 +4,4 @@ client_dir=$1
 
 cd $client_dir
 
-touch failed_login_data.txt
-
-cat */*/* | awk 'match($0, /([a-zA-Z0-9 ]+):..:.. computer_name sshd[.....]: Failed password for invalid user \([a-zA-Z]+) from \([0-9.]+) */, groups) {print groups[1] groups[2] groups[3] "\n"}' > failed_login_data.txt
-
-cat */*/* | awk 'match($0, /([a-zA-Z0-9 ]+):..:.. computer_name sshd[.....]: Failed password for \([a-zA-Z]+) from \([0-9.]+) */, groups) {print groups[1] groups[2] groups[3] "\n"}' > failed_login_data.txt
-
+cat */*/* | awk 'match($0, /([a-zA-Z]+) *([0-9]{1,2}) *([0-9]{2}):([0-9]{2}):([0-9]{2}) [a-zA-Z_]+ sshd\[([0-9]+)\]: Failed password for (invalid user )?([a-zA-Z0-9]+) from ([0-9.]+) port ([0-9]+) ssh2/, groups) {print  groups[1] groups[2] groups[3] groups[8] groups[9]}' > failed_login_data.txt
