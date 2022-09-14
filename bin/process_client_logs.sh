e5bd47945c3e9fa89a5fc83ab5a60f06a753840a
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-14 12:59:01 -0500

    Fix shellcheck errors and comment on process client log file

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# Take the directory that is being taken as an argument
 client_dir=$1 
 
-cd $client_dir
+# Go into the directory
+cd "$client_dir" || exit
 
-cat */*/* | awk 'match($0, /([a-zA-Z]+) *([0-9]{1,2}) *([0-9]{2}):[0-9]{2}:[0-9]{2} [a-zA-Z_]+ sshd\[[0-9]+]: Failed password for (invalid user )?([a-zA-Z0-9]+) from ([0-9.]+) port [0-9]+ ssh2/, groups) {print  groups[1] groups[2] groups[3] groups[5] groups[6]}' > failed_login_data.txt
+# Concatenate the files in the directory, read the lines and find those with failed password attempts. 
+# Of those lines, take the month, day, hour, username, and ip address and put them into a file: failed_login_data.text
+cat ./*/*/* | awk 'match($0, /([a-zA-Z]+) *([0-9]{1,2}) *([0-9]{2}):[0-9]{2}:[0-9]{2} [a-zA-Z_]+ sshd\[[0-9]+]: Failed password for (invalid user )?([a-zA-Z0-9]+) from ([0-9.]+) port [0-9]+ ssh2/, groups) {print   groups[1] groups[2] groups[3] groups[5] groups[6]}' > failed_login_data.txt
