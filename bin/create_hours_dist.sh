00cf5f02ad0530a1b4eb309c86cda4589b0d266d
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-15 14:53:51 -0500

    In the hours-of-the-day file, take a directory as the only argument and go into that directory

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -3,3 +3,8 @@
 # Make a temporary file for storing usernames
 Hour_file=$(mktemp)
 
+# Take the directory that we will be working out of as the only argument
+client_dir=$1
+
+# Go into the directory that has been taken as an argument
+cd "$client_dir" || exit
