11c7df9d6a1d8713ce7b3fa51b95f41d05c1e11f
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-15 16:02:59 -0500

    Sort the IP addresses so that the join command will work 	Note: added a second temp file

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Make a temporary file for storing country origin
+# Make two temporary files for storing country origin
 Country_file=$(mktemp)
+IP_file=$(mktemp)
 
 # Take the directory that we will be working out of as the only argument
 computers_file=$1
@@ -10,5 +11,6 @@ computers_file=$1
 cd "$computers_dir" || exit
 
 ## Concatenate the files containing failed log-in attempts from all computers
-## Extract the country of origin data in the fifth column
-cat ./*/failed_login_data.txt | awk '{print $5}' 
+## Extract the IP address data in the fifth column
+## Sort the IP address data so that the join function works
+cat ./*/failed_login_data.txt | awk '{print $5}' | sort > $IP_file 
