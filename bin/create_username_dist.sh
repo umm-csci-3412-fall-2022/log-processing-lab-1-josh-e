c21c8cb000db0f4b6b8b230fb785e4cc25dcb4b9
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-15 11:05:23 -0500

    Structure the output of failed login attempts as desired and put it into a temporary file

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -9,4 +9,8 @@ cd $client_dir
 ## Concatenate the files containing all failed log-in attempts
 ## Extract the username data in the fourth column 
 ## Sort the usernames so that the uniq command works
-cat ./*/failed_login_data.txt | awk '{print $4}' | sort
+## Count the number of occurances for each username
+## Structure the output in the form: data.addRow(['*username*', *occurances*});
+## Put it into a temp file
+cat ./*/failed_login_data.txt | awk '{print $4}' | sort | uniq -c | awk 'match($0, / *([0-9]+) +([a-zA-Z0-9_\-]+)/, groups) {print "data.addRow([\x27" groups[2] "\x27, " groups[1] "]);"}' > tempfile.txt
+
