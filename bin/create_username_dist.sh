ed87c89536d23e432e6e3b16ddedf4d7406f4be3
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-15 10:46:42 -0500

    Sort the extracted usernames of the failed log-in data to prepare them to be counted

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -4,6 +4,9 @@
 client_dir=$1 
 
 # Go into the directory that has been taken as an argument
-cd '$client_dir' || exit
+cd $client_dir
 
-cat */*/failed_login_data.txt
+## Concatenate the files containing all failed log-in attempts
+## Extract the username data in the fourth column 
+## Sort the usernames so that the uniq command works
+cat ./*/failed_login_data.txt | awk '{print $4}' | sort
