d4f6a559f55b118c70a933ab3769fcdb11e2e5c4
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-15 10:29:31 -0500

    In the file creating usernames, take a directory as an argument, go into that directory, and concatenate the file containing the failed log-in attempts.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# Take the directory that we will be working out of as the only argument
+client_dir=$1 
+
+# Go into the directory that has been taken as an argument
+cd '$client_dir' || exit
+
+cat */*/failed_login_data.txt
