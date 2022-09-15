96a6400bf2e5f795729c0cad08fe384b58bff54a
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-15 12:26:13 -0500

    Wrap the contents of the temporary file of usernames into a pie chart Also: 	-Made use of a temporary file function to ease calling the file 	-Deleted said temporary file afterwards

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Make a temporary file for storing usernames
+Username_file=$(mktemp)
+
 # Take the directory that we will be working out of as the only argument
 client_dir=$1 
 
@@ -11,6 +14,13 @@ cd $client_dir
 ## Sort the usernames so that the uniq command works
 ## Count the number of occurances for each username
 ## Structure the output in the form: data.addRow(['*username*', *occurances*});
-## Put it into a temp file
-cat ./*/failed_login_data.txt | awk '{print $4}' | sort | uniq -c | awk 'match($0, / *([0-9]+) +([a-zA-Z0-9_\-]+)/, groups) {print "data.addRow([\x27" groups[2] "\x27, " groups[1] "]);"}' > tempfile.txt
+## Put it into the temp file
+cat ./*/failed_login_data.txt | awk '{print $4}' | sort | uniq -c | awk 'match($0, / *([0-9]+) +([a-zA-Z0-9_\-]+)/, groups) {print "data.addRow([\x27" groups[2] "\x27, " groups[1] "]);"}' > $Username_file
+
+# Use wrap contents, with the temporary file as the text input and username distritribution 
+# headers/footers, into a pie chart .html
+../bin/wrap_contents.sh $Username_file ../html_components/username_dist ./username_dist.html
+
+# Delete the temporary file
+rm $Username_file
 
