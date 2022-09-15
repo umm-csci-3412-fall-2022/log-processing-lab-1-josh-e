d0558834f933c2be5e345cc7b7c1333b5cb5f646
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-15 15:38:24 -0500

    For the invalid log-in country file, take the directory as an argument and go into that directory

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -2,3 +2,9 @@
 
 # Make a temporary file for storing country origin
 Country_file=$(mktemp)
+
+# Take the directory that we will be working out of as the only argument
+Client_file=$1
+
+# Go into the directory that has been taken as an argument
+cd "$client_dir" || exit
