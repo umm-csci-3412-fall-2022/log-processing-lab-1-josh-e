ebc8203c72fca4708453a5df6421ada569ec4ab0
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-19 11:18:09 -0500

    Fix country distribution file to make it output the correct information

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -5,7 +5,7 @@ Country_file=$(mktemp)
 IP_file=$(mktemp)
 
 # Take the directory that we will be working out of as the only argument
-computers_file=$1
+computers_dir=$1
 
 # Go into the directory that has been taken as an argument
 cd "$computers_dir" || exit
@@ -13,7 +13,7 @@ cd "$computers_dir" || exit
 ## Concatenate the files containing failed log-in attempts from all computers
 ## Extract the IP address data in the fifth column
 ## Sort the IP address data so that the join function works
-cat ./*/failed_login_data.txt | awk '{print $5}' | sort > $IP_file
+cat ./*/failed_login_data.txt | awk '{print $5}' | sort > "$IP_file"
 
 ## Join temporary IP file with country IP address comparison group
 ## Extract the country data from the 2nd coloumn 
@@ -21,11 +21,11 @@ cat ./*/failed_login_data.txt | awk '{print $5}' | sort > $IP_file
 ## Count the number of occurances for each country
 ## Structure the output in the form: data.addRow(['*country*', *ocurrances*]);
 ## Put it into the country temp file
-join $IP_file ../etc/country_IP_map.txt | awk '{print $2}' | sort | uniq -c | awk 'match($0, / *([0-9]+) +([A-Z]{2})/, groups) {print "data.addRow([\x27" groups[2] "\x27, " groups[1] "]);"}' > $Country_file
+join "$IP_file" ../etc/country_IP_map.txt | awk '{print $2}' | sort | uniq -c | awk 'match($0, / *([0-9]+) *([A-Z]{2})/, groups) {print "data.addRow([\x27" groups[2] "\x27, " groups[1] "]);"}' > "$Country_file"
 
 # Use wrap contents, with the temporary file as the text input and country distribution 
 # headers/footers, into a map .html
-../bin/wrap_contents.sh "$Country_file" ../html_components/country_dist ./country_dist.html
+../bin/wrap_contents.sh $Country_file ../html_components/country_dist ./country_dist.html
 
 # Delete the temporary files
 rm "$IP_file"
