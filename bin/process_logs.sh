dd48010de0a45651b755139d52d38203e67f6318
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-20 13:07:30 -0500

    Go through all computer arguments in process logs: 	-Create directories in the temporary directory for each computer 	-un-gzip the information into their directories 	-Process the information we want from each of the computer's directories

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 # Create temporary directory for processing computer logs
-temp_dir=$(mktemp -d)
+Failed_Passwords_dir=$(mktemp -d)
+
+#For each argument (computer data):
+## Take the name of the computer
+## Make a directory in the temporary directory with the computer name as the name of the dirrectory
+## un-gzip the arguments and place them into their respective computer directories
+## Process the client logs in their computer directories
+for arg in "$@"
+	do 
+		Computer_dir=$(echo $arg | awk 'match($0, /\/([^.]+)\.tgz/, groups) {print groups[1] }' )
+		mkdir "$Failed_Passwords_dir"/"$Computer_dir"
+		tar -xzf $arg -C "$Failed_Passwords_dir"/"$Computer_dir"
+
+		./bin/process_client_logs.sh "$Failed_Passwords_dir"/"$Computer_dir"
+	done
 
