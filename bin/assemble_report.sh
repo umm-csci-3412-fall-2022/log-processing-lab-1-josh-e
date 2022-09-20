c6e97cdfd50a4c585fdb41ca00884b607ec25566
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-20 11:35:36 -0500

    Create a temp file in assemble_reports.sh and concatenate the wrapped data of username, country, and hour distributions into the temp file

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Create a temporary file for storing the reports
+Reports=$(mktemp)
+
 # Take the directory that we will be working out of as the only argument
 report_dir=$1
 
 # Go into the directory taken as the only argument
 cd "$report_dir" || exit
 
+cat ./country_dist.html hours_dist.html username_dist.html > "$Reports"
