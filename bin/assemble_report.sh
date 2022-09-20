76858bdd78c560cbceca327150c3aa122724d44a
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-20 11:31:49 -0500

    Take the directory we will be working in as an argument and go into that directory in assemble_report.sh

---
+++ bin/assemble_report.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Take the directory that we will be working out of as the only argument
+report_dir=$1
+
+# Go into the directory taken as the only argument
+cd "$report_dir" || exit
+
