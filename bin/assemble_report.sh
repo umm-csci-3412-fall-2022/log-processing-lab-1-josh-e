375a6078b15a51b9e0d958c23194d8a539d21774
Joshua Engelkes <jbengelkes@gmail.com>
2022-09-20 11:41:46 -0500

    Wrap the contents of the temporary file in assemble_report.sh into one singular .html

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -9,4 +9,10 @@ report_dir=$1
 # Go into the directory taken as the only argument
 cd "$report_dir" || exit
 
-cat ./country_dist.html hours_dist.html username_dist.html > "$Reports"
+# Concatenate the distribution data files in the argument directory
+# and put that into the reports temp file
+cat country_dist.html hours_dist.html username_dist.html > "$Reports"
+
+# Use wrap contents, with the temporary file as the text input and summary plots
+# headers/footers, into one collected .html
+../bin/wrap_contents.sh "$Reports" ../html_components/summary_plots failed_login_summary.html
